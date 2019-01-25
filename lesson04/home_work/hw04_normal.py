c3675876b40a946a475fe89a8ed77abba44be8cc
KuskovAM <ravenkus2@gmail.com>
2019-01-25 14:15:08 +0300

    test

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,8 +3,9 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
-line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+import re
+import random
+line1 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
        'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
@@ -19,7 +20,21 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+line = list(line1)
+for i in range(len(line)):
+       for k in range(65,92):
+              if line[i] == chr(k):
+                     line[i] = " "
+new_line = ''.join(line)
+new_line = new_line.split(' ')
 
+new_line = [x for x in new_line if x]
+print('Первое задание. Без re: ',new_line)
+##########################################################################
+
+result = re.split('[QWERTYUIOPASDFGHJKLZXCVBNM]', line1)
+result = [x for x in result if x]
+print('Первое задание. Через re: ',result)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -44,6 +59,49 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+newLine = []
+start = 0
+fin = 0
+
+def f(l): #Удаление повторяющихся эдементов списка
+    n = []
+    for i in l:
+        if i not in n:
+            n.append(i)
+    return n
+
+for i in range(len(line_2)):
+       if 'A' <= line_2[i] <='Z' and 'a' <= line_2[i-1] <= "z" and 'a' <= line_2[i-2] <= "z":
+              start = i
+              while 'A' <= line_2[i] <= 'Z':
+                     i += 1
+                     if i >= len(line_2):
+                            break
+              fin = i
+       if fin - start > 2:
+              subLine = line_2[start:fin - 2]
+              newLine.append(subLine)
+       i = fin
+       i += 1
+i = 1
+while i < len(newLine):
+       if newLine[i-1] == newLine[i]:
+              newLine.pop(i)
+       i += 1
+
+#Уверен, этот способ далеко не самый эффективный и всё можно сделать наааааамного проще
+
+newLine = f(newLine)
+print('Второе задание. Без re: ',newLine)
+#####################################################
+result = []
+pattern = re.findall('[qwertyuiopasdfghjklzxcvbnm]{2}[QWERTYUIOPASDFGHJKLZXCVBNM]{3,}', line_2)
+for i in pattern:
+    result.append(i[2:-2])
+
+
+print('Второе задание. Через re: ',result)
+
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -51,3 +109,53 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+import os
+
+f = open('text.txt', 'w')
+i = 0
+maxLength = 2500
+
+a = random.randint(1,9)
+a = str(a)
+f.write(a)
+while i < maxLength-1:
+    a = random.randint(0,9)
+    a = str(a)
+    f.write(a)
+    i += 1
+f.close()
+f = open('text.txt', 'r')
+for line in f:
+    print(line)
+d = {}
+length = len(line)
+f.close()
+for i in range(len(line)-1):
+    length = 0
+    if i == len(line):
+        break
+    if line[i] == line[i+1]:
+        start = i
+        num = line[i]
+        length  = 2
+
+
+        while line[i] == line[i+1]:
+            length += 1
+            i +=1
+        max = length-1
+        d.update({max:i})
+print(d)
+keys = d.keys()
+key = list(keys)
+key.sort()
+max = key[len(key)-1]
+
+pos = d.get(max)
+
+current = line[pos - max +1 : pos+1]
+print(current)
+
+
+
+
