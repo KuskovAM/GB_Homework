2b1f8e1c6652d9cafe2c9220346490e75e06b868
HOMEP_9 <ravenkus2@gmail.com>
2019-01-28 16:47:23 +0300

    1-3

--- lesson05/home_work/easy.py
+++ lesson05/home_work/easy.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import shutil
 print('sys.argv = ', sys.argv)
+print(os.getcwd()+os.path.basename(sys.argv[0]))
 
 def print_help():
     print("help - получение справки")
     print("mkdir - создание директорий dir_1 - dir_9")
     print("deldir - удаляет директории dir_1 - dir_9")
+    print("dirlist - список директрии рабочей папки")
 
 def make_dir():
     i = 1
@@ -33,21 +36,39 @@ def del_dir():
             print('директория {} отсутствует'.format(dir_name))
         i += 1
 
+def dir_list():
+    dList = []
+    dir_path = os.getcwd()
+    for i in os.listdir(dir_path):
+        if os.path.isdir(i) == True:
+            dList.append(i)
+
+    print('папки: ', dList)
+def copy_file():
+    dir_path = os.path.join(os.getcwd(), os.path.basename(sys.argv[0]))
+    new_file_path = os.path.join(os.getcwd(), 'new_file.py')
+    print(dir_path)
+    shutil.copyfile(str(dir_path), str(new_file_path))
+
 do = {
     "help": print_help,
     "mkdir": make_dir,
-    "deldir": del_dir
+    "deldir": del_dir,
+    "dirlist": dir_list,
+    "copyfile": copy_file
 }
 
-#make_dir()
+# make_dir()
 # del_dir()
-try:
-    key = sys.argv[1]
-except IndexError:
-    key = None
-if key:
-    if do.get(key):
-        do[key]()
-    else:
-        print("Задан неверный ключ")
-        print("Укажите ключ help для получения справки")
+#dir_list()
+copy_file()
+# try:
+#     key = sys.argv[1]
+# except IndexError:
+#     key = None
+# if key:
+#     if do.get(key):
+#         do[key]()
+#     else:
+#         print("Задан неверный ключ")
+#         print("Укажите ключ help для получения справки")
